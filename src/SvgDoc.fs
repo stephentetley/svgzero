fd2e7d787a35a850a665908a45c301ed25e012ee
stephentetley <stephen.tetley@gmail.com>
2016-08-09 22:35:31 +0100

    Initial commit

---
+++ src/SvgDoc.fs
@@ -0,0 +1,39 @@
+﻿namespace SvgZero
+
+open System.Xml
+open System.Xml.Linq
+
+
+module SvgDoc = 
+    let nssvg : XNamespace = XNamespace.Get("http://www.w3.org/2000/svg")
+
+    let svgElem (name : string) (attrs : seq<XAttribute>) = new XElement (nssvg + name, attrs )  
+
+    let svgElemB (name : string) (attrs : seq<XAttribute>) (elems : seq<XElement>) = 
+        new XElement (XName.Get name, attrs, elems)  
+
+    let svgElemNoAttrs (name : string) (elems : seq<XElement>) = 
+        new XElement (XName.Get name, elems)  
+    
+
+    let elemSvg (body : seq<XElement>) : XElement = 
+        let nsxlink : XNamespace = XNamespace.Get("http://www.w3.org/1999/xlink")
+        let top = new XElement ( XName.Get ("svg", nssvg.NamespaceName ), 
+                                 new XAttribute(XName.Get "version", "1.1"),
+                                 new XAttribute(XNamespace.Xmlns + "xlink", nsxlink))
+        top.Add body
+        top
+                               
+
+    let attrFill (s : string) : XAttribute = new XAttribute(XName.Get "fill", s )
+
+    let attrId (s : string) : XAttribute = new XAttribute(XName.Get "id", s )
+
+    let attrR (d : double) : XAttribute = new XAttribute(XName.Get "r", d.ToString() )
+    let attrRx (d : double) : XAttribute = new XAttribute(XName.Get "rx", d.ToString() )
+    let attrRy (d : double) : XAttribute = new XAttribute(XName.Get "ry", d.ToString() )
+
+    let attrCx (d : double) : XAttribute = new XAttribute(XName.Get "cx", d.ToString() )
+    let attrCy (d : double) : XAttribute = new XAttribute(XName.Get "cy", d.ToString() )
+
+    let elemCircle (attrs : seq<XAttribute>) : XElement = svgElem "circle" attrs
---
+++ src/Test.fsx
@@ -0,0 +1,43 @@
+﻿// open System;;
+// Environment.CurrentDirectory <- @"E:\coding\fsharp\svgzero\svgzero";;
+// #I @"E:\coding\fsharp\svgzero\svgzero";;
+// #load @"Test.fsx";;
+// open Test;;
+
+#r "System.Xml"
+#r "System.Xml.Linq"
+
+#load "SvgDoc.fs"
+
+open SvgZero
+open System.Xml
+open System.Xml.Linq
+
+let test01 = SvgDoc.elemSvg [ SvgDoc.elemCircle <| seq [ SvgDoc.attrCx 30.0; SvgDoc.attrCy 80.0; SvgDoc.attrR 20.0; SvgDoc.attrFill "blue" ] ]
+
+let doc = new XmlDocument ()
+let nssvg : XNamespace = XNamespace.Get("http://www.w3.org/2000/svg")
+let nsxlink : XNamespace = XNamespace.Get("http://www.w3.org/1999/xlink")
+
+let linkname : XName = nsxlink.GetName("xlink")
+
+let root = new XElement ( XName.Get ("svg", "http://www.w3.org/2000/svg")) 
+
+let root2 = new XElement ( XName.Get ("svg", "http://www.w3.org/2000/svg"), 
+                           new XAttribute(XName.Get "version", "1.1")
+                         ) 
+
+let root3 = new XElement ( XName.Get ("svg", nssvg.NamespaceName ), 
+                           new XAttribute(XName.Get "version", "1.1"),
+                           new XAttribute(XNamespace.Xmlns + "xlink", nsxlink)
+                         ) 
+
+
+let doc2 = 
+   let d1 = new XDocument (new XDeclaration("1.0", "utf-8", "true")) 
+   d1.Add root3
+   d1
+    
+
+let testy01 = doc.ToString() 
+let testy02 = doc2.ToString() 
